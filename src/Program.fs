cd4d651bc2dcdccffd31f6039302f66354a70fa9
Jon Roberts <himself@bigjon.dev>
2026-09-09 18:29:27 -0500

    Revert "Generate nested records for arrays of untyped inline objects (fixes #32) (#36)"
    This reverts commit d256d36a19259ffdd0558763e6d2fe7c46242099.

--- src/Program.fs
+++ src/Program.fs
@@ -899,7 +899,7 @@ let rec createRecordFromSchema (recordName: string) (schema: OpenApiSchema) (vis
         let isObjectArray =
             propertyType.Type = "array"
             && isNotNull propertyType.Items
-            && isObjectSchema propertyType.Items
+            && propertyType.Items.Type = "object"
             && isNull propertyType.Items.Reference
             && isNotNull propertyType.Items.Properties
             && propertyType.Items.Properties.Count > 0
